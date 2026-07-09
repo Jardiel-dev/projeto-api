b612cd0a67ef262cc0b8d2ed5859ac5f5676ed09
Jardiel <cordeirodiel@gmail.com>
2026-07-09 19:18:56 -0300

    Refatora conexão com API usando função

--- src/extract.py
+++ src/extract.py
@@ -3,25 +3,35 @@ import pandas as pd
 
 from dotenv import load_dotenv
 import os
-import json
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
 url = "https://api.football-data.org/v4/competitions/2013/scorers"
+
 headers = {
     "X-Auth-Token": API_KEY
 }
 
-print("Conectando à API...")
 
-response = requests.get(url, headers=headers)
+def conectar_api(url):
+    print("Conectando à API...")
 
-if response.status_code == 200:
-    print("Conexão realizada com sucesso!")
+    response = requests.get(url, headers=headers)
 
-    dados = response.json()
+    if response.status_code == 200:
+        print("Conexão realizada com sucesso!")
+        return response.json()
+
+    print(f"Erro: {response.status_code}")
+    print(response.text)
+    return None
+
+
+dados = conectar_api(url)
+
+if dados:
 
     artilheiros = dados["scorers"]
 
@@ -32,11 +42,3 @@ if response.status_code == 200:
     tabela.to_csv("data/raw/artilheiros.csv", index=False)
 
     print("\nArquivo salvo em data/raw/artilheiros.csv")
-
-    tabela.to_csv("data/raw/partidas.csv", index=False)
-
-    print("\nArquivo salvo em data/raw/partidas.csv")
-
-else:
-    print(f"Erro: {response.status_code}")
-    print(response.text)
